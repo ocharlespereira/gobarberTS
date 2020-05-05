7b8a76197fe93d16f2ef2d51113d2667e2e88e04
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-05 20:37:13 -0300

    first commit

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+// instanciando a classe criada no repositories
+const appointmentsRepository = new AppointmentsRepository();
+
+// SoC: Separation of Concerns (Separação de preocupações)
+
+/**
+ * Movido para o appointmentsRepository
+ // Esta falando que o appointments é um array de Appointments do interface do models (TS)
+ const appointments: Appointment[] = [];
+ */
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default appointmentsRouter;

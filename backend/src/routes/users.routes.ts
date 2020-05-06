0f2f8c6a8010b3e0d211ae43e4e366e4fb9e7074
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-06 15:54:11 -0300

    configurando upload de arquivos com o methodo PATCH

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const UsersRouter = Router();
+const upload = multer(uploadConfig);
 
 UsersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +27,14 @@ UsersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+UsersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default UsersRouter;

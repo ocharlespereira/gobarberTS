877af754335e0ed832037a00c3db99fa89faef99
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-06 16:12:42 -0300

    Atualizar avatar e a imagem da pasta de temporarios

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const UsersRouter = Router();
@@ -33,7 +35,20 @@ UsersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   },
 );
 

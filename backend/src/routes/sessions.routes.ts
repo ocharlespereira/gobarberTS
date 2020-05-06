5bb5d738baa48b11a29d41eb538427c99ffe9aec
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-06 00:42:25 -0300

    sessions valaidaçào de credenciais de acesso

---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const SessionsRouter = Router();
+
+SessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default SessionsRouter;

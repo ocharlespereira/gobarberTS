f51e5d02326780796ffc2298aeb2111ef505298f
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-06 16:42:59 -0300

    Listando errors 401 e 500 no server.ts

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -11,23 +11,19 @@ const UsersRouter = Router();
 const upload = multer(uploadConfig);
 
 UsersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
+  return response.json(user);
 });
 
 UsersRouter.patch(
@@ -35,20 +31,16 @@ UsersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFileName: request.file.filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (error) {
-      return response.status(400).json({ error: error.message });
-    }
+    return response.json(user);
   },
 );
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,40 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadCofing from './config/upload';
+
+import AppError from './errors/AppError';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+
+// liberar utilizaçao das imagens
 app.use('/files', express.static(uploadCofing.directory));
+
 app.use(routes);
+
+// tratativa de errors
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port in 3333!');

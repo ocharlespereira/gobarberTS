1a0219318f1924015b8a3ab98aff68276637c4e7
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-05 23:01:52 -0300

    Criaçao de migrations de alteraçao de coluna e relacionamento nos models

---
+++ backend/src/database/migrations/1588728759955-AlterProviderFieldToProviderId.ts
@@ -0,0 +1,52 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AlterProviderFieldToProviderId1588728759955
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    /**
+     * RESTRICT -> Nao deixa usuario ser deletado se existir appointments
+     * SET NULL -> set a coluna provider_id como nulo caso o usuário seja deletado
+     * CASCADE -> Deletou o usuário deleta todos os seus appointments
+     */
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+
+    await queryRunner.dropColumn('appointments', 'provider_id');
+
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      }),
+    );
+  }
+}
--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -4,7 +4,19 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+/**
+ * Um para Um (OneToOne)
+ * Um para Muitos (OneToMany)
+ * Muitos para Muitos (ManyToMany)
+ * ********************
+ * JoinColumn -> coluna que vai identificar o usuário prestador
+ */
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -12,7 +24,12 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  // relacionamento
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;

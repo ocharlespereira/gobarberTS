0a7e338b73c7fc176669f677c66132e303bf412e
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-05 21:11:28 -0300

    criando model de agendamento com typeORM

--- backend/src/database/migrations/1588689487318-CreateAppointments.ts
+++ backend/src/database/migrations/1588689487318-CreateAppointments.ts
@@ -1,11 +1,34 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1588689487318 implements MigrationInterface {
+export default class CreateAppointments1588689487318
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appoitments');
+  }
 }

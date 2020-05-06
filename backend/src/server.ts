b2be5fd49e1144364feba303ac6d0cd78d603a98
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-06 16:18:03 -0300

    Disponibilizando arquivos via link estatico no server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadCofing from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadCofing.directory));
 app.use(routes);
 
 app.listen(3333, () => {

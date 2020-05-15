7b9c13f643ffad987025f6438dea62ab77f82156
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-14 21:37:48 -0300

    frontend: pagina de login

--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished'
 
 import signInBackground from '../../assets/sign-in-background.png'
 
@@ -7,16 +8,88 @@ export const Container = styled.div`
 
   display: flex;
   align-items: stretch;
-
 `;
+
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
 
-  place-content: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+  align-items: center;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #F4EDE8;
+
+      &::placeholder{
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      height: 56px;
+      background: #ff9000;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #312e38;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#ff9000')};
+      }
+    }
+
+    a {
+      color: #F4EDE8;
+      display: block;
+      margin-top: 24px;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')}
+      }
+    }
+  }
+
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+    margin-right: 16px;
+
+    &:hover {
+        color: ${shade(0.2, '#ff9000')};
+      }
+  }
+
 `;
 export const Background = styled.div`
   flex:1;

b5419b86f90aac2f7e15921d24624a94cb60bae4
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-17 20:58:46 -0300

    Configurando rotas de autenticação

---
+++ frontend/src/routes/Route.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouterProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouterProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  console.log(user, isPrivate);
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+            <Redirect
+              to={{
+                pathname: isPrivate ? '/' : '/dashboard',
+                state: { from: location },
+              }}
+            />
+          );
+      }}
+    />
+  );
+};
+
+export default Route;

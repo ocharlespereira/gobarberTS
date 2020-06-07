37a16109c254510a5a9d7888fbd8b7ee07289726
Charles Pereira <charlesti.ptu@gmail.com>
2020-06-07 19:50:36 -0300

    Configure eslint e prettier

---
+++ mobile/prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, //aspas simples
+  trailingComma: 'all', //virgula no final de arrays
+  allowParens: 'avoid', //nao colocar parenteses q tenham arrowfuncions de uma linha
+}

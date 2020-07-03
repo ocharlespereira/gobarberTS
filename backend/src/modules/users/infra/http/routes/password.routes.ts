707a8dfb96ec58d4afaa9c7a2bb98f7a931a0061
Charles Pereira <charlesti.ptu@gmail.com>
2020-07-02 22:44:06 -0300

    Criacao de rotas e o resetPassword

--- backend/src/modules/users/infra/http/routes/password.routes.ts
+++ backend/src/modules/users/infra/http/routes/password.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import ForgotPasswordController from '../controller/ForgotPasswordController';
+import ResetPasswordController from '../controller/ResetPasswordController';
 
 const PasswordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 PasswordRouter.post('/forgot', forgotPasswordController.create);
+PasswordRouter.post('/reset', resetPasswordController.create);
 
 export default PasswordRouter;

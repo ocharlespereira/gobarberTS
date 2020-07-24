e9e856554a61333f65e2fe3056c4d526a3da1c97
Charles Pereira <charlesti.ptu@gmail.com>
2020-07-23 22:41:18 -0300

    Update profile

---
+++ backend/src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,47 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+import uploadConfig from '@config/upload';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  user_id: string;
+  avatarFileName: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ user_id, avatarFileName }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findbyId(user_id);
+
+    if (!user) {
+      throw new AppError('Only authenticated users can change avatar.', 401);
+    }
+
+    if (user.avatar) {
+      await this.storageProvider.deleteFile(user.avatar);
+    }
+
+    const filename = await this.storageProvider.saveFile(avatarFileName);
+
+    user.avatar = filename;
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateProfileService;

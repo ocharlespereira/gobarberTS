e0af1a713c0d4e863492263346823fedffbce583
Charles Pereira <charlesti.ptu@gmail.com>
2020-07-24 23:07:55 -0300

    profile atualization

--- backend/src/modules/users/services/UpdateProfileService.ts
+++ backend/src/modules/users/services/UpdateProfileService.ts
@@ -1,12 +1,12 @@
 import { injectable, inject } from 'tsyringe';
 
-// import User from '../infra/typeorm/entities/User';
+import User from '../infra/typeorm/entities/User';
 // import uploadConfig from '@config/upload';
 
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 interface IRequest {
   user_id: string;
@@ -28,6 +28,10 @@ class UpdateProfile {
 
   public async execute({ user_id, name, email }: IRequest): Promise<User> {
     const user = await this.usersRepository.findbyId(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
 
     return user;
   }

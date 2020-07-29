4ee919a8670699de6a3e22bcb2087e493f18779e
Charles Pereira <charlesti.ptu@gmail.com>
2020-07-28 23:40:56 -0300

    Route controller profile

---
+++ backend/src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,29 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ShowProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findbyId(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+  }
+}
+
+export default ShowProfileService;

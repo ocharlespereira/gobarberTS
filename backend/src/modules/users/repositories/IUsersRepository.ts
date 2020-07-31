af2584baeec0f33a2fc914d290d0d48ef2a38ac6
Charles Pereira <charlesti.ptu@gmail.com>
2020-07-30 22:49:52 -0300

    Listing providerst method

--- backend/src/modules/users/repositories/IUsersRepository.ts
+++ backend/src/modules/users/repositories/IUsersRepository.ts
@@ -2,6 +2,7 @@ import User from '../infra/typeorm/entities/User';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
 export default interface IUsersRepository {
+  findAllProvider(except_user_id?: string): Promise<User[]>;
   findbyId(id: string): Promise<User | undefined>;
   findbyEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;

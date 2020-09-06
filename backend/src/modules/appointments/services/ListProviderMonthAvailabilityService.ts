99d388aa4bcd3bc3bf5810cf16471e290c10d241
Charles Pereira <charlesti.ptu@gmail.com>
2020-09-06 00:14:20 -0300

    listprovider month available

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -9,11 +9,18 @@ interface IRequest {
   year: number;
 }
 
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
 @injectable()
 class ListProviderMonthAvailabilityService {
   constructor() {}
 
-  public async execute({ user_id, month, year }: IRequest): Promise<void> {}
+  public async execute({ user_id, month, year }: IRequest): Promise<IResponse> {
+    return [{ day: 1, available: false }];
+  }
 }
 
 export default ListProviderMonthAvailabilityService;

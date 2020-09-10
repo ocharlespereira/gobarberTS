7d44c60a1aba2e8bcd4e274fee2b38d2ed2f05eb
Charles Pereira <charlesti.ptu@gmail.com>
2020-09-09 22:36:50 -0300

    import dtos IFindAllInDayFromProviderDTO in method

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
@@ -8,5 +9,7 @@ export default interface IAppointmentsRepository {
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
-  findAllInDayFromProvider(): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProviderDTO,
+  ): Promise<Appointment[]>;
 }

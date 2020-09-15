b5b4603f128960810d96b0a644136397eff1b01c
Charles Pereira <charlesti.ptu@gmail.com>
2020-09-14 23:03:03 -0300

    routes controllers

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controller/ProvidersController';
+import ProviderMonthAvailabilityController from '../controller/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controller/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get('/:id/month-availability', providersController.index);
+providersRouter.get('/:id/day-availability', providersController.index);
 
 export default providersRouter;

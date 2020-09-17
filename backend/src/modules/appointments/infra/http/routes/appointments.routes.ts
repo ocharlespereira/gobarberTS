13f4db44882da25f758cdf65d89ae1b9fedf6233
Charles Pereira <charlesti.ptu@gmail.com>
2020-09-17 00:21:12 -0300

    create route appointmentsController list

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controller/AppointmentsController';
+import ProviderAppointmentController from '../controller/ProviderAppointmentController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentController = new ProviderAppointmentController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentController.index);
 
 export default appointmentsRouter;

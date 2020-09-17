7b0ef24dcb2d2bf3ecb5b0edfe2b7777c45fc5c2
Charles Pereira <charlesti.ptu@gmail.com>
2020-09-16 23:54:27 -0300

    provider schedule create appointment

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private AppointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.AppointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+        day,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentService;

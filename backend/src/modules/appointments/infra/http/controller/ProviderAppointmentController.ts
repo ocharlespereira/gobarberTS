d3c015d01d475cf4903eb9debaa2c0bbf1a55259
Charles Pereira <charlesti.ptu@gmail.com>
2020-09-17 00:17:10 -0300

    controller providerAppointments List

---
+++ backend/src/modules/appointments/infra/http/controller/ProviderAppointmentController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderAppointmentService from '@modules/appointments/services/ListProviderAppointmentService';
+
+export default class ProviderAppointmentController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { day, month, year } = request.body;
+
+    const listProviderAppointments = container.resolve(
+      ListProviderAppointmentService,
+    );
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(appointments);
+  }
+}

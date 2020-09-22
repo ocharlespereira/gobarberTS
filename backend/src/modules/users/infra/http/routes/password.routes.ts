e34e469b32f0538f7a448a273e959b21263c5bb3
Charles Pereira <charlesti.ptu@gmail.com>
2020-09-21 22:00:59 -0300

    validate data celebrate, validation paramns and body

--- backend/src/modules/users/infra/http/routes/password.routes.ts
+++ backend/src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ForgotPasswordController from '../controller/ForgotPasswordController';
 import ResetPasswordController from '../controller/ResetPasswordController';
@@ -7,7 +8,25 @@ const PasswordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-PasswordRouter.post('/forgot', forgotPasswordController.create);
-PasswordRouter.post('/reset', resetPasswordController.create);
+PasswordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+PasswordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().uuid().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create,
+);
 
 export default PasswordRouter;

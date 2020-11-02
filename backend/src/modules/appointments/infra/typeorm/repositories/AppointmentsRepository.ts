57203b5892141f51aa8ca737dc2bbb636df09cde
Charles Pereira <charlesti.ptu@gmail.com>
2020-11-02 00:32:49 -0300

    method corrections backend

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -62,6 +62,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;

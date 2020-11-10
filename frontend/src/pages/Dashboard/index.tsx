43203316cb4e67bc467cf723d396c63ed8c847d0
Charles Pereira <charlesti.ptu@gmail.com>
2020-11-10 01:38:54 -0300

    Frontend - create integration

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { FiPower } from 'react-icons/fi';
 
+import { useAuth } from '../../hooks/auth';
+
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Header, HeaderContent, Profile } from './styles';
 
-const Dashboard: React.FC = () => (
-  <Container>
-    <Header>
-      <HeaderContent>
-        <img src={logoImg} alt="Gobarber" />
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
 
-        <Profile>
-          <img
-            src="https://avatars2.githubusercontent.com/u/54192694?s=460&u=a0ac6a9b16621a72fd3bfd6bba0c0081c2259d5b&v=4"
-            alt="Charles Pereira"
-          />
-          <div>
-            <span>Bem-vindo</span>
-            <strong>Charles Pereira</strong>
-          </div>
-        </Profile>
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="Gobarber" />
 
-        <button type="button">
-          <FiPower />
-        </button>
-      </HeaderContent>
-    </Header>
-  </Container>
-);
+          <Profile>
+            <img src={user?.avatar_url} alt={user?.name} />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user?.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;

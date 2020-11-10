d32bc437b8e998f7ef27c30efbea8efe222cbc04
Charles Pereira <charlesti.ptu@gmail.com>
2020-11-10 01:22:31 -0300

    Frontend - create dashboard and styles

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Container></Container>;
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+const Dashboard: React.FC = () => (
+  <Container>
+    <Header>
+      <HeaderContent>
+        <img src={logo} alt="Gobarber" />
+
+        <Profile>
+          <img
+            src="https://avatars2.githubusercontent.com/u/54192694?s=460&u=a0ac6a9b16621a72fd3bfd6bba0c0081c2259d5b&v=4"
+            alt="Charles Pereira"
+          />
+          <div>
+            <span>Bem-vindo</span>
+            <strong>Charles Pereira</strong>
+          </div>
+        </Profile>
+
+        <button type="button">
+          <FiPower />
+        </button>
+      </HeaderContent>
+    </Header>
+  </Container>
+);
 
 export default Dashboard;
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Header = styled.div``;
+
+export const HeaderContent = styled.div``;
+
+export const Profile = styled.div``;

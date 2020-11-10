d256c1b61535e1925d8aee1f5b4e7b2a7ae5b9d8
Charles Pereira <charlesti.ptu@gmail.com>
2020-11-10 01:18:20 -0300

    Frontend - create dashboard and styles

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => <Container></Container>;
 
 export default Dashboard;

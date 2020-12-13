32559c7368ae5eba6ed1f27e590d2ee08bbe6ec0
Charles Pereira <charlesti.ptu@gmail.com>
2020-12-13 16:50:23 -0300

    Mobile - page dashboard

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -12,9 +12,10 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
 
-interface Provider {
+export interface Provider {
   id: string;
   name: string;
   avatar_url: string;
@@ -56,6 +57,12 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user?.avatar_url || imgDefault }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={(provider) => provider?.id}
+        renderItem={({ item }) => <UserName>{item?.name}</UserName>}
+      />
     </Container>
   );
 };

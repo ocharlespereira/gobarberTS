d2e25fac9666f9d834e343ca3645588adccfb21f
Charles Pereira <charlesti.ptu@gmail.com>
2020-12-16 22:23:04 -0300

    Mobile - page CreateAppointments

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import { useRoute } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
 
 interface RouteParams {
   providerId: string;
 }
 
+const imgDefault =
+  'https://avatars2.githubusercontent.com/u/54192694?s=460&u=a0ac6a9b16621a72fd3bfd6bba0c0081c2259d5b&v=4';
+
 const CreateAppointment: React.FC = () => {
+  const { user } = useAuth();
   const route = useRoute();
   const { providerId } = route.params as RouteParams;
 
@@ -20,6 +32,10 @@ const CreateAppointment: React.FC = () => {
         <BackButton onPress={() => {}}>
           <Icon name="chevron-left" size={20} color="#999591" />
         </BackButton>
+
+        <HeaderTitle>Cabeleiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user?.avatar_url || imgDefault }} />
       </Header>
     </Container>
   );
--- mobile/src/pages/CreateAppointment/styles.ts
+++ mobile/src/pages/CreateAppointment/styles.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Header = styled.View``;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text``;
+
+export const UserAvatar = styled.Image``;

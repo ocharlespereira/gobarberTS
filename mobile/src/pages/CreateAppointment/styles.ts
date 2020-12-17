2dace63bc5b4ba5aa7958799556986201526b273
Charles Pereira <charlesti.ptu@gmail.com>
2020-12-16 22:25:35 -0300

    Mobile - page CreateAppointments

--- mobile/src/pages/CreateAppointment/styles.ts
+++ mobile/src/pages/CreateAppointment/styles.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
   padding: 24px;
   padding-top: ${getStatusBarHeight() + 24}px;
   background: #28262e;
@@ -11,10 +15,18 @@ export const Container = styled.View`
   align-items: center;
 `;
 
-export const Header = styled.View``;
-
 export const BackButton = styled.TouchableOpacity``;
 
-export const HeaderTitle = styled.Text``;
+export const HeaderTitle = styled.Text`
+  color: #f5ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 20px;
+  margin-left: 16px;
+`;
 
-export const UserAvatar = styled.Image``;
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
+`;

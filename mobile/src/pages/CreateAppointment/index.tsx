614c9886e14a4482751a1a6d020c0955489f7c05
Charles Pereira <charlesti.ptu@gmail.com>
2020-12-16 22:17:20 -0300

    Mobile - page CreateAppointments

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { Container } from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+
+  console.log('routeParams', providerId);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}}>
+          <Icon name="chevron-left" size={20} color="#999591" />
+        </BackButton>
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;

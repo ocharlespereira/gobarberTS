26777b814e5d2a1505542d6315cfc37d8c218263
Charles Pereira <charlesti.ptu@gmail.com>
2021-01-06 10:14:33 -0300

    Mobile - page CreateAppointmentCreat

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluido</Title>
+      <Description>
+        Quarta, dia 06 de Janeiro de 2021 ás 11:00h com Charles Pereira
+      </Description>
+
+      <OkButton onPress={() => {}}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
---
+++ mobile/src/pages/AppointmentCreated/styles.ts
@@ -0,0 +1,17 @@
+import { RectButton } from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 24px;
+`;
+
+export const Title = styled.View``;
+
+export const Description = styled.Text``;
+
+export const OkButton = styled(RectButton)``;
+
+export const OkButtonText = styled.Text``;

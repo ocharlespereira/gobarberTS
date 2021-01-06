946732cdc6f09467048dce4f85d0b573403d746a
Charles Pereira <charlesti.ptu@gmail.com>
2021-01-05 21:53:53 -0300

    Mobile - page CreateAppointments Calendar button appointment

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Alert } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -85,7 +85,7 @@ const imgDefault =
 const CreateAppointment: React.FC = () => {
   const { user } = useAuth();
   const route = useRoute();
-  const { goBack } = useNavigation();
+  const { goBack, navigate } = useNavigation();
 
   const routeParams = route.params as RouteParams;
 
@@ -147,6 +147,27 @@ const CreateAppointment: React.FC = () => {
     },
     [],
   );
+
+  const handleCreateAppointment = useCallback(async () => {
+    try {
+      const date = new Date(selectedDate);
+
+      date.setHours(selectedHour);
+      date.setMinutes(0);
+
+      await api.post('appointments', {
+        provider_id: selectedProvider,
+        date,
+      });
+
+      navigate('AppointmentCreated', { date: date.getTime() });
+    } catch (error) {
+      Alert.alert(
+        'Erro ao criar um agendamento',
+        'Ocorreu um erro ao tentar criar um agendamento, tente novamente.',
+      );
+    }
+  }, [navigate, selectedDate, selectedHour, selectedProvider]);
 
   const morningAvailability = useMemo(() => {
     return availability
@@ -276,7 +297,7 @@ const CreateAppointment: React.FC = () => {
           </Section>
         </Schedule>
 
-        <CreateAppointmentButton onPress={() => {}}>
+        <CreateAppointmentButton onPress={handleCreateAppointment}>
           <CreateAppointmentButtonText>Agendar</CreateAppointmentButtonText>
         </CreateAppointmentButton>
       </Content>

7e2b216ed274f9b42312947489dfc04b63403882
Charles Pereira <charlesti.ptu@gmail.com>
2021-01-26 00:59:23 -0300

    Mobile - configure setupTests.ts

--- mobile/src/__tests__/pages/SignIn.spec.tsx
+++ mobile/src/__tests__/pages/SignIn.spec.tsx
@@ -3,6 +3,12 @@ import { render, fireEvent } from '@testing-library/react-native';
 
 import SignIn from 'src/pages/SignIn';
 
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+
 describe('SignIn page', () => {
   it('should contains email/password input', () => {
     const { getByPlaceholderText } = render(<SignIn />);
